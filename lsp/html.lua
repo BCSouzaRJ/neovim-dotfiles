8be480b3815f279b5c462622ae848c25edc0e88f
Beatriz C. Souza <beatriz.csouza.rj@gmail.com>
2026-07-08 13:49:46 -0300

    impedindo htmllint de funcionar dentro de projetos angular

--- lsp/html.lua
+++ lsp/html.lua
@@ -1,5 +1,17 @@
 return {
-  cmd = { 'vscode-html-language-server', '--stdio' },
-  filetypes = { 'html' },
-  root_markers = { 'package.json', '.git' },
+    cmd = { 'vscode-html-language-server', '--stdio' },
+    filetypes = { 'html' },
+    -- Substituímos a lista fixa por uma função que valida o projeto
+    root_dir = function(fname)
+        -- Importa o utilitário nativo de caminhos do lspconfig
+        local util = require('lspconfig.util')
+
+        -- Se encontrar arquivos específicos do Angular, aborta o carregamento deste LSP
+        if util.root_pattern('angular.json', 'nx.json')(fname) then
+            return nil
+        end
+
+        -- Caso contrário, mantém o comportamento padrão procurando seus marcadores
+        return util.root_pattern('package.json', '.git')(fname)
+    end,
 }
